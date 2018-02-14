62195055828e468caf176971abf3a7c49f59995e
Vladimir <smirnovv91@gmail.com>
2018-02-14 17:53:21 +0300

    fixed main func, added print_site_status func

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,5 +1,5 @@
 import requests
-import whois #python-whois
+import whois
 import datetime
 import sys
 
@@ -29,12 +29,23 @@ def get_domain_expiration_date(domain_name, days=30):
     else:
         return False
 
+def print_site_status(url, server_availability, expiration_date):
+    print("URL:", url)
+    if server_availability:
+        print("[OK] Server is avaliavle")
+    else:
+        print("[X] Server is not avaliable")
+    if expiration_date:
+        print("[OK] Expiration date is more than a month")
+    else:
+        print("[X] Expiration date is less than a month")
+    print()
+
 
 if __name__ == "__main__":
     try:
         file_path = sys.argv[1]
         urls_list = load_urls4check(file_path)
-
     except IndexError:
         print("Arguments error")
         sys.exit()
@@ -44,14 +55,4 @@ if __name__ == "__main__":
     for url in urls_list:
         server_availability = is_server_respond_with_200(url)
         expiration_date = get_domain_expiration_date(url)
-        print(expiration_date)
-        print("URL:", url)
-        if server_availability:
-            print("[OK] Server is avaliavle")
-        else:
-            print("[X] Server is not avaliable")
-        if expiration_date:
-            print("[OK] Expiration date is more than a month")
-        else:
-            print("[X] Expiration date is less than a month")
-        print()
+        print_site_status(url, server_availability, expiration_date)
